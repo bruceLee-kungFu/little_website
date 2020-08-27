1ba2af8d81ce11f460b57af527dc6b9ccda33718
Bruce Lee <you@example.com>
2020-08-27 22:06:33 +0200

    livereload, w3.css

--- config.rb
+++ config.rb
@@ -5,6 +5,10 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+configure :development do
+  set :debug_assets, false
+  activate :livereload
+end
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
